80626975369cf558dd103020b3f86f59c1a3795b
amangarg078 <amangarg078@gmail.com>
2017-03-11 16:26:03 +0530

    Add Streaming 1

--- app/views.py
+++ app/views.py
@@ -60,7 +60,6 @@ def stream(file):
 
     with open(path,'rb') as open_file:
         yield open_file.read()
-        time.sleep(0.5)
 
 # function to handle filestreams of images and videos
 def vid(request, filepost):
